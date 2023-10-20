bc49bcc80585b31e43644d51b1c7ce5ce9315b09
nadia.nobre <nadiamsnobre@gmail.com>
2023-10-20 19:35:18 +0100

    correct file

--- sql/V01__Alter_function.sql
+++ sql/V01__Alter_function.sql
@@ -1,6 +1,7 @@
-CREATE TABLE [IF NOT EXISTS] testTable (
+CREATE TABLE testTable (
    column1 datatype(length) column_contraint,
    column2 datatype(length) column_contraint,
    column3 datatype(length) column_contraint,
    table_constraints
+   
 );
