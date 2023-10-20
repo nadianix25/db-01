211bff211ac914b4365a41201f40c1eb670fb3c3
nadia.nobre <nadiamsnobre@gmail.com>
2023-10-20 19:37:35 +0100

    Correct script

--- sql/V01__Alter_function.sql
+++ sql/V01__Alter_function.sql
@@ -1,7 +1,3 @@
 CREATE TABLE testTable (
-   column1 datatype(length) column_contraint,
-   column2 datatype(length) column_contraint,
-   column3 datatype(length) column_contraint,
-   table_constraints
-   
+   column1 varchar(50) 
 );
